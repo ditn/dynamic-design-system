923d0cf40c762e902cad88cc8d979d11bc0da4c5
Adam Bennett <adam.bennett@cuvva.com>
2020-11-12 09:58:31 +0000

    Moshi kapt -> ksp

--- design-system-frontend/settings.gradle.kts
+++ design-system-frontend/settings.gradle.kts
@@ -1,2 +1,18 @@
 rootProject.name = "design-system-frontend"
 include(":app")
+
+pluginManagement {
+    resolutionStrategy {
+        eachPlugin {
+            when (requested.id.id) {
+                "symbol-processing" ->
+                    useModule("com.google.devtools.ksp:symbol-processing:${requested.version}")
+            }
+        }
+    }
+
+    repositories {
+        gradlePluginPortal()
+        google()
+    }
+}
